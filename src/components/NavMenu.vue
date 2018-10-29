0002f3bb6ea3e98630484e7c9fa6b3c2cba06775
t-ishii <perlispython.plp@gmail.com>
2018-10-29 15:53:21 +0900

    Some modify

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-menu :default-active="activeIndex" :router="true" mode="horizontal">
+    <li role="menuitem" class="el-menu-item"><el-input 
+      placeholder="Keyword"
+      suffix-icon="el-icon-search"></el-input></li>
+    <el-menu-item index="/">Home</el-menu-item>
+    <el-menu-item index="/about">About</el-menu-item>
+  </el-menu>
+</template>
+<script>
+export default {
+  name: 'NavMenu',
+  data () {
+    return {
+      activeIndex: 'home'
+    }
+  }
+}
+</script>
