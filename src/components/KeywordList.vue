e4c86698dbf8e2aa14fa0a952f73e404a6226fa7
t-ishii <perlispython.plp@gmail.com>
2018-10-30 15:36:34 +0900

    Add delete confirm dialog

--- src/components/KeywordList.vue
+++ src/components/KeywordList.vue
@@ -1,14 +1,28 @@
 <template>
-  <el-table :data="keywords" style="width: 100%;">
-    <el-table-column prop="keyword" label="keyword"></el-table-column>
-    <el-table-column prop="description" label="description"></el-table-column>
-    <el-table-column label="">
-      <template slot-scope="scope">
-        <el-button icon="el-icon-delete" circle @click="handleClickDelete(scope.$index, scope.row)"></el-button>
-        <el-button icon="el-icon-edit" circle @click="handleClickEdit(scope.$index, scope.row)"></el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div class="keyword-list">
+    <el-table :data="keywords" style="width: 100%;">
+      <el-table-column prop="keyword" label="keyword"></el-table-column>
+      <el-table-column prop="description" label="description"></el-table-column>
+      <el-table-column label="">
+        <template slot-scope="scope">
+          <el-button icon="el-icon-delete" circle @click="handleClickDelete(scope.$index, scope.row)"></el-button>
+          <el-button icon="el-icon-edit" circle @click="handleClickEdit(scope.$index, scope.row)"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog
+      :visible.sync="deleteDialogVisible"
+      title="削除確認"
+      width="30%">
+      <span>{{ deleteKeyword }}を削除してもよろしいですか？</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="deleteDialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="deleteDialogVisible = false">OK</el-button>
+      </span>
+    </el-dialog>
+
+  </div>
 </template>
 
 <script>
@@ -16,6 +30,8 @@ export default {
   name: 'KeywordList',
   data() {
     return {
+      deleteDialogVisible: false,
+      deleteKeyword: '',
       keywords: [
         {
           keyword: 'TEST',
@@ -38,6 +54,8 @@ export default {
   },
   methods: {
     handleClickDelete(index, row) {
+      this.deleteKeyword = row.keyword
+      this.deleteDialogVisible = true
       console.log([index, row])
     },
     handleClickEdit(index, row) {
