f9549b9c09b8d198bb8a81da3546fe8edf0a57e7
t-ishii <perlispython.plp@gmail.com>
2018-10-30 09:39:00 +0900

    Create markdown preview

---
+++ src/components/Preview.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="preview" v-html="preview">
+  </div>
+</template>
+
+<script>
+import showdown from 'showdown'
+
+export default {
+  name: 'Preview',
+  props: {
+    text: ''
+  },
+  computed: {
+    preview() {
+      const converter = new showdown.Converter()
+      return converter.makeHtml(this.text)
+    }
+  }
+}
+</script>
--- src/views/NewDict.vue
+++ src/views/NewDict.vue
@@ -1,20 +1,30 @@
 <template>
-  <el-form :model="form" ref="form" :rules="formRule" label-width="120px" class="create-form">
-    <el-form-item label="Keyword" :required="true" prop="keyword">
-      <el-input v-model="form.keyword"></el-input>
-    </el-form-item>
-    <el-form-item label="Description" :required="true" prop="description">
-      <el-input type="textarea" v-model="form.description" :rows="5"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">Create</el-button>
-    </el-form-item>
-  </el-form>
+  <el-tabs v-model="activeName">
+    <el-tab-pane label="form" name="form">
+      <el-form :model="form" ref="form" :rules="formRule" label-width="120px" class="create-form">
+        <el-form-item label="Keyword" :required="true" prop="keyword">
+          <el-input v-model="form.keyword"></el-input>
+        </el-form-item>
+        <el-form-item label="Description" :required="true" prop="description">
+          <el-input type="textarea" v-model="form.description" :rows="5"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Create</el-button>
+        </el-form-item>
+      </el-form>
+    </el-tab-pane>
+    <el-tab-pane label="preview" name="preview">
+      <preview :text="form.description"></preview>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
+import Preview from '@/components/Preview.vue'
+
 export default {
   name: 'NewDict',
+  components: { Preview },
   data() {
 
     const validateEmpty = (rule, value, callback) => {
@@ -26,6 +36,7 @@ export default {
     }
 
     return {
+      activeName: 'form',
       form: {
         keyword: '',
         description: ''
