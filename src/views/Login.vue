60331d5f56fd259ae0ae8aa9b16c6c38579598ac
t-ishii <perlispython.plp@gmail.com>
2018-10-31 10:51:15 +0900

    Add validate check to login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form :model="form" :rules="formRule" label-width="120px" class="login">
+  <el-form :model="form" :rules="formRule" label-width="120px" class="login" ref="loginForm">
     <el-form-item label="Email" prop="email">
       <el-input v-model="form.email"></el-input>
     </el-form-item>
@@ -15,6 +15,9 @@
 <script>
 import validate from '@/utils/validator'
 
+import firebase from 'firebase'
+import 'firebase/auth'
+
 export default {
   name: 'Login',
   data() {
@@ -24,14 +27,28 @@ export default {
         password: ''
       },
       formRule: {
-        email: [{ validator: validate.validateEmpty, trigger: 'blur' }],
+        email: [
+          { validator: validate.validateEmpty, trigger: 'blur' },
+          { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change']}
+        ],
         password: [{ validator: validate.validateEmpty, trigger: 'blur' }]
       }
     }
   },
   methods: {
     submitForm() {
-      this.$router.push({name: 'home'})
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          firebase.auth()
+          .signInWithEmailAndPassword(this.form.email, this.form.password)
+          .then((userCredential) => {
+            console.log(userCredential.credential)
+            this.$router.push({name: 'home'})
+          })
+        } else {
+          return false
+        }
+      })
     }
   }
 }
