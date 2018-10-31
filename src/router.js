1e568defe3d0e23354dbcbc3cd3d7f91f481a18d
t-ishii <perlispython.plp@gmail.com>
2018-10-31 09:47:35 +0900

    Add login page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import EditDict from './views/EditDict.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/new',
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-form :model="form" :rules="formRule" label-width="120px" class="login">
+    <el-form-item label="Email" prop="email">
+      <el-input v-model="form.email"></el-input>
+    </el-form-item>
+    <el-form-item label="Password" prop="password">
+      <el-input type="password" v-model="form.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm">Login</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import validate from '@/utils/validator'
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      formRule: {
+        email: [{ validator: validate.validateEmpty, trigger: 'blur' }],
+        password: [{ validator: validate.validateEmpty, trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$router.push({name: 'home'})
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  max-width: 500px;
+}
+</style>
