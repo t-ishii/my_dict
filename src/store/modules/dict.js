c33c255b66136ba01a404f22be3023a2cafd0340
t-ishii <perlispython.plp@gmail.com>
2018-11-01 22:47:21 +0900

    WIP

--- src/store/modules/dict.js
+++ src/store/modules/dict.js
@@ -1,5 +1,9 @@
+import MyDict from '@/utils/Mydict'
+
+const DEFAULT_LIMIT = 10
+
 const state = {
-  all: []
+  dicts: []
 }
 
 const getters = {
@@ -7,11 +11,21 @@ const getters = {
 }
 
 const mutations = {
-
+  update(state, dicts) {
+    for (dict of dicts) {
+      state.dicts.push(dict)
+    }
+  }
 }
 
 const actions = {
-
+  limitToFirst({ commit }, uid, n = DEFAULT_LIMIT) {
+    MyDict.limitToFirst(uid, n).then(snapshot => {
+      if (Array.isArray(snapshot.val())) {
+        commit('update', snapshot.val())
+      }
+    })
+  }
 }
 
 export default {
