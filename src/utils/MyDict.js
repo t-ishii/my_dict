f40570b72839038a3ace6ab9a89531f98d3ae336
t-ishii <perlispython.plp@gmail.com>
2018-11-03 00:38:42 +0900

    Refactoring

--- src/utils/MyDict.js
+++ src/utils/MyDict.js
@@ -1,24 +1,26 @@
 import firebase from 'firebase'
 import 'firebase/database'
 
+const database = () => firebase.database()
+
 const MyDict = {
   listen(uid, callback) {
-    firebase.database().ref(`dict/${uid}`).on('value', callback)
+    database.ref(`dict/${uid}`).on('value', callback)
   },
   unListen(uid, callback) {
-    firebase.database().ref(`dict/${uid}`).off('value', callback)
+    database.ref(`dict/${uid}`).off('value', callback)
   },
   all(uid) {
-    return firebase.database().ref(`dict/${uid}`).once('value')
+    return fdatabase.ref(`dict/${uid}`).once('value')
   },
   insert(uid, dict) {
-    return firebase.database().ref(`dict/${uid}/${dict.keyword}`).update(dict)
+    return database.ref(`dict/${uid}/${dict.keyword}`).update(dict)
   },
   update(uid, dict) {
-    return firebase.database().ref(`dict/${uid}/${dict.keyword}`).set(dict)
+    return database.ref(`dict/${uid}/${dict.keyword}`).set(dict)
   },
   delete(uid, keyword) {
-    return firebase.database().ref(`dict/${uid}/${keyword}`).remove()
+    return database.ref(`dict/${uid}/${keyword}`).remove()
   }
 }
 
